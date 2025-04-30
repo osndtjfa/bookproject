ad52e28c83175844de49f547ddcff24cfa3e8bcc
osndtjfa <ennenka@gmail.com>
2025-04-30 11:39:06 +0900

    third

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 #add
 MEDIA_URL = '/media/'  #好きな名前を変更できる　Ex、image

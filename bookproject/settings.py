69c6c3a7821ff30c2abd920c15596c08b8b55999
osndtjfa <ennenka@gmail.com>
2025-04-30 13:19:11 +0900

    five

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^h_on+6+)kk8z4wyq#0-#(e4t#4-#fau%+%q(t5f3j!-hlt5v)'
+# SECRET_KEY = 'django-insecure-^h_on+6+)kk8z4wyq#0-#(e4t#4-#fau%+%q(t5f3j!-hlt5v)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
